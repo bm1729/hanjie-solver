aa5438207a4a81e6c91a297e0bc07d1f218c65ce
bm1729 <ben.mumford1729@gmail.com>
2015-12-17 16:15:12 +0000

    Added test

--- src/test/java/com/benmumford/algorithm/CommonRowColumnFinderTests.java
+++ src/test/java/com/benmumford/algorithm/CommonRowColumnFinderTests.java
@@ -1,5 +1,42 @@
 package com.benmumford.algorithm;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import com.benmumford.data.Cell;
+import com.benmumford.data.RowColumn;
+
 public class CommonRowColumnFinderTests {
     
+    private CommonRowColumnFinder target;
+    
+    @Before
+    public void beforeEach() {
+        target = new CommonRowColumnFinder();
+    }
+    
+    private RowColumn createRowColumn(Cell... cells) {
+        RowColumn toReturn = new RowColumn(cells.length);
+        for (int i = 0; i < cells.length; ++i) {
+            toReturn = toReturn.applyValue(i, cells[i]);
+        }
+        
+        return toReturn;
+    }
+    
+    @Test
+    public void findCommonRowColumn() {
+        ArrayList<RowColumn> candidates = new ArrayList<>();
+        candidates.add(createRowColumn(Cell.FULL, Cell.EMPTY, Cell.FULL));
+        candidates.add(createRowColumn(Cell.FULL, Cell.EMPTY, Cell.EMPTY));
+        
+        RowColumn result = target.findCommonRowColumn(candidates);
+        
+        Assert.assertEquals(Cell.FULL, result.getValue(0));
+        Assert.assertEquals(Cell.EMPTY, result.getValue(1));
+        Assert.assertEquals(Cell.NULL, result.getValue(2));
+    }
 }
